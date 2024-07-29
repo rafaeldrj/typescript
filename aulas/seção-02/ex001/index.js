825ff44e8055d2ad0a73288a57ee0f1ca38c7bb6
Rafael Gonçalves <rafael.drj@outlook.com>
2024-07-29 10:25:17 -0300

    console.log

--- aulas/seção-02/ex001/index.js
+++ aulas/seção-02/ex001/index.js
@@ -1,6 +1,5 @@
-console.log('Rafael Gonçalves'); // String = Texto
-console.log("Rafael Gonçalves"); // Number = Numero
-console.log(`Rafael Gonçalves`);
+console.log("Rafael Gonçalves") // String = Texto
+console.log("Rafael Gonçalves") // Number = Numero
+console.log(`Rafael Gonçalves`)
 
-console.log(35, 15.85, 'Rafael Gonçalves')
-console.log("Ola Mundo!")
+console.log(35, 15.85, "Rafael Gonçalves")
